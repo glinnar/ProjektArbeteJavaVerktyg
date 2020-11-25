2203c2fbf3b11ea79d3cbfd82edc4a70440fe4b0
Athanasios <athana.stathis@gmail.com>
2020-11-25 15:42:20 +0100

    1st Commit-Class Contact

--- src/main/java/com/Projektarbete/Contact/Contact.java
+++ src/main/java/com/Projektarbete/Contact/Contact.java
@@ -2,5 +2,47 @@ package com.Projektarbete.Contact;
 
 public class Contact {
 
+    private String firstName;
+    private String lastName;
+    private int telephoneNumber;
+    private String emailAdress;
 
+    public Contact(String firstName, String lastName, int telephoneNumber, String emailAdress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.telephoneNumber = telephoneNumber;
+        this.emailAdress = emailAdress;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(int telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getEmailAdress() {
+        return emailAdress;
+    }
+
+    public void setEmailAdress(String emailAdress) {
+        this.emailAdress = emailAdress;
+    }
 }
