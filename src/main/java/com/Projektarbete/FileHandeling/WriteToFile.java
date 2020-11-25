6bf073d7715df01b66991e613e9b40b76f307adc
Gurra <gurra@DESKTOP-5GTVRCK.localdomain>
2020-11-25 11:10:14 +0100

    Added list examplefile

--- src/main/java/com/Projektarbete/FileHandeling/WriteToFile.java
+++ src/main/java/com/Projektarbete/FileHandeling/WriteToFile.java
@@ -1,4 +1,31 @@
 package com.Projektarbete.FileHandeling;
 
+import java.io.File;
+
 public class WriteToFile {
+    private File contactList = null;
+    private String filePath;
+    private boolean fileExist = false;
+
+    public void createFile(){
+        try{
+            contactList = new File("C:");
+            fileExist = contactList.isFile();
+            filePath = contactList.getPath();
+            System.out.println("is file? " + fileExist);
+
+            contactList = new File("Test.txt");
+            fileExist
+
+
+
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
+    }
+
+
 }
--- src/main/java/com/Projektarbete/Main/Main.java
+++ src/main/java/com/Projektarbete/Main/Main.java
@@ -1,8 +1,12 @@
 package com.Projektarbete.Main;
 
+import com.Projektarbete.FileHandeling.WriteToFile;
+
 public class Main {
 
     public static void main(String[] args) {
+        WriteToFile filewriter = new WriteToFile();
+        filewriter.createFile();
 
     }
 }
