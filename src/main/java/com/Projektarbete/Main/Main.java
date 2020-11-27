1a44457befad0189c933b8eedd3a4b63124d4014
Kozue Yamada <kozue.yamada@iths.se>
2020-11-27 16:12:11 +0100

    Work in progress

--- src/main/java/com/Projektarbete/Main/Main.java
+++ src/main/java/com/Projektarbete/Main/Main.java
@@ -3,6 +3,16 @@ package com.Projektarbete.Main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test by Kozue");
+
+    }
+
+    private static void printActions() {
+        System.out.println("===== MENU =====");
+        System.out.println("0: Close Contact Boo" +
+                "1: Show All Contacts\n" +
+                "2: Add New Contact\n" +
+                "3: Update Existing Contact\n" +
+                "4: Search Existing Contact\n" +
+                "5: Delete Existing Contact");
     }
 }
