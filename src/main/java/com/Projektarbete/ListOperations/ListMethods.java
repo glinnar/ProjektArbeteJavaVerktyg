cc8c656a954f031fa3d80f66b985860d4769936a
Hans Sandblom <hans.sandblom@iths.se>
2020-11-27 10:37:14 +0100

    Added class for the list, search, add and remove methods

---
+++ src/main/java/com/Projektarbete/ListOperations/ListMethods.java
@@ -0,0 +1,81 @@
+package com.Projektarbete.ListOperations;
+
+import java.util.ArrayList;
+
+public class ListMethods {
+
+    private String myFirstName;
+    private String myLastName;
+    private String myMail;
+    private String myPhone;
+    private ArrayList<ContactList> myContacts;
+
+    // Konstruktor
+    public ListMethods(String myFirstName, String myLastName, String myMail, String myPhone) {
+        this.myFirstName = myFirstName;
+        this.myLastName = myLastName;
+        this.myMail = myMail;
+        this.myPhone = myPhone;
+        this.myContacts = new ArrayList<>();
+    }
+
+    private int findContact(ContactList contact) {
+        return this.myContacts.indexOf(contact);
+    }
+
+    private int findContact(String contactName) {
+        for (int i = 0; i < this.myContacts.size(); i++) {
+            ContactList contact = this.myContacts.get(i);
+            if (contact.getFirstName().equals(contactName) || contact.getLastName().equals(contactName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // När ny person läggs till sker kontroll på att den inte redan finns i arraylistan
+    // Kontrollen görs enbart på för- och efternamn.
+    public boolean addNewContact(ContactList contact,int index, boolean update) {
+        if (findContact(contact.getFirstName()) >= 0 && findContact(contact.getLastName()) >= 0 && !update) {
+            System.out.println("Contact already exist.");
+            return false;
+        }
+
+        if (update) {
+            myContacts.add((index-1), contact);
+        }
+        else {
+            myContacts.add(contact);
+        }
+        return true;
+
+    }
+
+    public boolean deleteContact(int index) {
+
+        if (myContacts.size()>0 && myContacts.size()>(index-1)) {
+            myContacts.remove(index - 1);
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    public void printContacts() {
+        System.out.println("List of contacts:");
+        for (int i = 0; i < this.myContacts.size(); i++) {
+            System.out.println((i + 1) + "." +
+                    this.myContacts.get(i).getFirstName() + " " +
+                    this.myContacts.get(i).getLastName() + " " +
+                    this.myContacts.get(i).getMail() + " " +
+                    this.myContacts.get(i).getPhone());
+        }
+    }
+
+    public void searchContact() {
+        // Added later
+    }
+
+}
