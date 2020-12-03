b5abe5b0d1cb2269482de4f427caa92e770d2158
Gurra <gurra@DESKTOP-5GTVRCK.localdomain>
2020-12-03 09:36:33 +0100

    Fixed Merge conflict

--- src/main/java/com/Projektarbete/Main/Main.java
+++ src/main/java/com/Projektarbete/Main/Main.java
@@ -1,11 +1,7 @@
 package com.Projektarbete.Main;
 
-import com.Projektarbete.Contact.Contact;
 import com.Projektarbete.FileHandeling.WriteToFile;
 import com.Projektarbete.ListOperations.ContactList;
-
-import java.io.IOException;
-import java.util.ArrayList;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,29 +10,14 @@ import java.util.Scanner;
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
+
     public static void main(String[] args) throws IOException {
-       // String Msg = scanner.nextLine();
-        ArrayList<Integer> list = new ArrayList();
-        for (int i = 0; i < 50; i++) {
-            list.add(i);
-        }
-
-        WriteToFile write = new WriteToFile();
-
-
-<<<<<<< HEAD
-    public static void main(String[] args) throws IOException {
-        ContactList contact = new ContactList("Gurra","Kungen","kungen@kungen.se","0700-112233");
+        ContactList contact = new ContactList("Gurra", "Kungen", "kungen@kungen.se", "0700-112233");
         ArrayList<ContactList> list = new ArrayList<>();
         list.add(contact);
         System.out.println(list);
         WriteToFile filewriter = new WriteToFile();
         filewriter.createFile();
-=======
->>>>>>> fa3f65b449b59ff7120b99e32cfc16c70fb8816c
-
-        filewriter.writeDataToFile(list);
-
 
 
     }
