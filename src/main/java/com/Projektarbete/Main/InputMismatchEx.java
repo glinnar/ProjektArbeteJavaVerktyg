19990f5bf07f29dbdb08f8afa1c9c4ce26581876
Kozue Yamada <kozue.yamada@iths.se>
2020-12-10 17:48:43 +0100

    Updated Main package

---
+++ src/main/java/com/Projektarbete/Main/InputMismatchEx.java
@@ -0,0 +1,22 @@
+package com.Projektarbete.Main;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class InputMismatchEx {
+
+    public void enterInteger() {
+        {
+            Scanner scanner = new Scanner(System.in);
+            try {
+                System.out.println("Starting program... ");
+                System.out.println("Press any num key and enter: ");
+                Integer input = scanner.nextInt();
+                System.out.println(input);
+            } catch (InputMismatchException ex) {
+                throw new InputMismatchException("\"Error - Press any num key and enter: ");
+            }
+        }
+    }
+}
--- src/main/java/com/Projektarbete/Main/Main.java
+++ src/main/java/com/Projektarbete/Main/Main.java
@@ -5,6 +5,7 @@ import com.Projektarbete.FileHandeling.WriteToFile;
 import com.Projektarbete.ListOperations.ContactList;
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -21,16 +22,78 @@ public class Main {
         contactList = fileReader.readFromFile();
 
 
+
+        boolean waitingForCorrectInput = true;
+
+        while (waitingForCorrectInput) {
+            InputMismatchEx keyEnter = new InputMismatchEx();
+            try {
+                keyEnter.enterInteger();
+                waitingForCorrectInput = false;
+            } catch (InputMismatchException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+
+
+        boolean quit = false;
+        System.out.println("<<< CONTACT BOOK >>>");
+        printActions();
+
+        while (!quit) {
+            System.out.print("Choose from menu (6 to show menu): ");
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (action) {
+                case 0:
+                    System.out.println("\nClosing...");
+                    quit = true;
+                    break;
+
+                case 1:
+                    contactList.printContacts();
+                    break;
+
+                case 2:
+                    //addNewContact();
+                    break;
+
+                case 3:
+                    //updateContact();
+                    break;
+
+                case 4:
+                    //removeContact();
+                    break;
+
+                case 5:
+                    //queryContact();
+                    break;
+
+                case 6:
+                    printActions();
+                    break;
+
+                default:
+                    System.out.println("Choose number from menu.");
+                    break;
+
+            }
+        }
     }
 
+
+
     private static void printActions() {
-        System.out.println("===== MENU =====");
-        System.out.println("0: Close Contact Boo" +
+        System.out.println("========== MENU ==========");
+        System.out.println("0: Close Contact Book\n" +
                 "1: Show All Contacts\n" +
                 "2: Add New Contact\n" +
                 "3: Update Existing Contact\n" +
                 "4: Search Existing Contact\n" +
                 "5: Delete Existing Contact");
+        System.out.println("========== **** ==========");
     }
 
 }
